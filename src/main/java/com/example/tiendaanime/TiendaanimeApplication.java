76353c62ca2217294fa580fcadef57d7ffa785b2
jaiber <antoniogarcia200603@gmail.com>
2023-09-11 19:37:36 -0500

    creando repositorio

---
+++ src/main/java/com/example/tiendaanime/TiendaanimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiendaanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaanimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaanimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tiendaanime/TiendaanimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiendaanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaanimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
